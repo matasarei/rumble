669ad685e8ad90f3490ac4c7a96074810e240141
Jordan Cook <jordan@jc.me.uk>
2019-02-16 12:39:01 +0000

    Fixed case in marhsaller import

--- src/Command/SeedCommand.php
+++ src/Command/SeedCommand.php
@@ -3,7 +3,7 @@
 namespace Matasar\Bundle\Rumble\Command;
 
 use Matasar\Bundle\Rumble\Resolver;
-use Aws\DynamoDB\Marshaler;
+use Aws\DynamoDb\Marshaler;
 use Aws\DynamoDb\DynamoDbClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
